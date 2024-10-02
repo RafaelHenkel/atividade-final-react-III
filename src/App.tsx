e9ec7fa466b0dfc46d9193940e8cb23833384ffa
Rafael Henkel <rafael0henkel1@gmail.com>
2024-10-01 22:30:55 -0300

    feat: add `redux` and `redux persistor`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Provider } from 'react-redux';
 import AppRoutes from './routes/AppRoutes';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/store';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <AppRoutes />
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
 
 export default App;
