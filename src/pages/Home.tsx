2faf2332eb37780497c566eb49135854e5fbd4cd
Rafael Henkel <rafael0henkel1@gmail.com>
2024-10-01 22:52:51 -0300

    feat: add `searchBar`

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,12 @@ import PageDefault from '../config/layout/PageDefault';
 import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import { Grid2 as Grid } from '@mui/material';
 import PokeCard from '../components/PokeCard';
+import { Paper } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
 
 export function Home() {
   function handleNext() {
@@ -15,6 +21,19 @@ export function Home() {
     <>
       <PageDefault>
         <Grid container spacing={2} paddingY="15px">
+          <Grid size={12} display="flex" justifyContent="center">
+            <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '50%' }}>
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Pesquisar pokemon"
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+              <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            </Paper>
+          </Grid>
           <Grid size={3}>
             <PokeCard />
           </Grid>
