e80c3bcc5cd9a7a78641e96e1172892bcb07c93b
Rafael Henkel <rafael0henkel1@gmail.com>
2024-10-15 19:38:38 -0300

    feat: remove images `PokePage`

--- src/pages/PokePage.tsx
+++ src/pages/PokePage.tsx
@@ -81,47 +81,6 @@ export function PokePage() {
                 {selector?.poke.types.map(item => (
                   <li key={item.type.name} className="flex justify-center items-center gap-2">
                     {item.type.name}
-                    <img
-                      src={
-                        item.type.name === 'flying'
-                          ? './public/fly.png'
-                          : item.type.name === 'dark'
-                            ? './public/dark.png'
-                            : item.type.name === 'dragon'
-                              ? './public/dragon.png'
-                              : item.type.name === 'earth'
-                                ? './public/earth.png'
-                                : item.type.name === 'fairy'
-                                  ? './public/fairy.png'
-                                  : item.type.name === 'figther'
-                                    ? './public/figther.png'
-                                    : item.type.name === 'fire'
-                                      ? './public/fire.png'
-                                      : item.type.name === 'ghost'
-                                        ? './public/ghost.png'
-                                        : item.type.name === 'grass'
-                                          ? './public/grass.png'
-                                          : item.type.name === 'ice'
-                                            ? './public/ice.png'
-                                            : item.type.name === 'normal'
-                                              ? './public/normal.png'
-                                              : item.type.name === 'psyc'
-                                                ? './public/psyc.png'
-                                                : item.type.name === 'rock'
-                                                  ? './public/rock.png'
-                                                  : item.type.name === 'steel'
-                                                    ? './public/steel.png'
-                                                    : item.type.name === 'thunder'
-                                                      ? './public/thunder.png'
-                                                      : item.type.name === 'water'
-                                                        ? './public/water.png'
-                                                        : item.type.name === 'ground'
-                                                          ? './public/ground.png'
-                                                          : item.type.name === 'poison'
-                                                            ? './public/poison.png'
-                                                            : undefined
-                      }
-                    />
                   </li>
                 ))}
               </ul>
@@ -129,24 +88,6 @@ export function PokePage() {
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
               {selector?.poke.stats.map(item => (
                 <div key={item.stat.name} className="flex flex-col w-full justify-center items-center">
-                  <img
-                    className="w-10 h-10"
-                    src={
-                      item.stat.name === 'hp'
-                        ? './public/heart.png'
-                        : item.stat.name === 'attack'
-                          ? './public/x-attack.png'
-                          : item.stat.name === 'defense'
-                            ? './public/x-defense.png'
-                            : item.stat.name === 'special-attack'
-                              ? './public/x-sp-atk.png'
-                              : item.stat.name === 'special-defense'
-                                ? './public/x-sp-def.png'
-                                : item.stat.name === 'speed'
-                                  ? './public/x-speed.png'
-                                  : undefined
-                    }
-                  />
                   <Typography variant="subtitle1">{item.stat.name}</Typography>
                   <Typography variant="subtitle2">{item.base_stat}</Typography>
                 </div>
