f05e8b6d3482b0698aaf8a64df1bc509f7f1cbe5
Rafael Henkel <rafael0henkel1@gmail.com>
2024-10-17 19:21:47 -0300

    feat: change type colors

--- src/pages/PokePage.tsx
+++ src/pages/PokePage.tsx
@@ -60,12 +60,53 @@ export function PokePage() {
   }, [name, selector]);
 
   function handleLike() {
-    dispach(likedPoke(selector.poke));
+    dispach(selector?.poke.types);
   }
 
-  function handleClieck(pokemon) {
-    sliderClasses(pokemon);
-  }
+  const getColorByType = (type: string) => {
+    switch (type) {
+      case 'bug':
+        return '#76a866';
+      case 'dark':
+        return '#60596b';
+      case 'dragon':
+        return '#7038f8';
+      case 'electric':
+        return '#f9be00';
+      case 'fairy':
+        return '#f469a9';
+      case 'fighting':
+        return '#c03028';
+      case 'fire':
+        return '#f08030';
+      case 'flying':
+        return '#768fd1';
+      case 'ghost':
+        return '#735797';
+      case 'grass':
+        return '#48d0b0';
+      case 'ground':
+        return '#f2cd5a';
+      case 'ice':
+        return '#77d2f7';
+      case 'normal':
+        return '#5c5c5c';
+      case 'poison':
+        return '#a040a0';
+      case 'psychic':
+        return '#f542bd';
+      case 'rock':
+        return '#bcac66';
+      case 'steel':
+        return '#b8b8d0';
+      case 'water':
+        return '#457af7';
+      default:
+        return 'gray';
+    }
+  };
+
+  console.log(selector?.poke.abilities);
 
   return (
     <>
@@ -100,43 +141,58 @@ export function PokePage() {
               <img src={selector?.poke.sprites.back_shiny} alt={selector?.poke.name} />
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <Typography variant="h3">{selector?.poke.name}</Typography>
+              <Typography color="white" variant="h3">
+                {selector?.poke.name}
+              </Typography>
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <Typography variant="subtitle1">ID: {selector?.poke.id}</Typography>
+              <Typography color="white" variant="subtitle1">
+                ID: {selector?.poke.id}
+              </Typography>
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <Typography variant="subtitle1">Altura: {selector?.poke.height} Metros</Typography>
+              <Typography color="white" variant="subtitle1">
+                Altura: {selector?.poke.height} Metros
+              </Typography>
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <Typography variant="subtitle1">Habilidades:</Typography>
+              <Typography color="white" variant="subtitle1">
+                Habilidades:
+              </Typography>
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
               <ul className="flex flex-col items-center justify-center">
-                {selector?.poke.abilities.map(item => <li key={item.ability.name}>{item.ability.name}</li>)}
-              </ul>
-            </Grid>
-            <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <Typography variant="subtitle1">Tipo:&nbsp;</Typography>
-            </Grid>
-            <Grid size={12} display="flex" justifyContent="center" alignItems="center">
-              <ul className="flex flex-col items-center justify-center">
-                {selector?.poke.types.map(item => (
-                  <Typography onClick={() => handleClieck(item)}>
-                    <li key={item.type.name} className="flex justify-center items-center gap-2">
-                      <Chip label={item.type.name} sx={{ bgcolor: pokeColor }}>
-                        {item.type.name}
-                      </Chip>
-                    </li>
+                {selector?.poke.abilities.map(item => (
+                  <Typography color="white" component="li" key={item.ability.name}>
+                    {item.ability.name}
                   </Typography>
                 ))}
               </ul>
             </Grid>
             <Grid size={12} display="flex" justifyContent="center" alignItems="center">
+              <Typography color="white" variant="subtitle1">
+                Tipo:&nbsp;
+              </Typography>
+            </Grid>
+            <Grid size={12} display="flex" justifyContent="center" alignItems="center">
+              {selector?.poke.types.map(item => (
+                <Chip
+                  key={item.type.name}
+                  label={item.type.name}
+                  sx={{ bgcolor: getColorByType(item.type.name) }}
+                  style={{ color: 'white', marginBottom: '0.5rem' }}
+                />
+              ))}
+            </Grid>
+            <Grid size={12} display="flex" justifyContent="center" alignItems="center">
               {selector?.poke.stats.map(item => (
                 <div key={item.stat.name} className="flex flex-col w-full justify-center items-center">
-                  <Typography variant="subtitle1">{item.stat.name}</Typography>
-                  <Typography variant="subtitle2">{item.base_stat}</Typography>
+                  <Typography color="white" variant="subtitle1">
+                    {item.stat.name}
+                  </Typography>
+                  <Typography color="white" variant="subtitle2">
+                    {item.base_stat}
+                  </Typography>
                 </div>
               ))}
             </Grid>
